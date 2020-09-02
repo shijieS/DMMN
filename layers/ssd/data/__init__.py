a6441963165832491c99ace30fff12486f6b97b4
shijie <shijieSun@chd.edu.cn>
2020-09-03 01:44:15 +0800

    remove wrong license

--- layers/ssd/data/__init__.py
+++ layers/ssd/data/__init__.py
@@ -1,11 +1,4 @@
-#  #!/usr/bin/env python
-#   Copyright (c) 2019. ShiJie Sun at the Chang'an University
-#   This work is licensed under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License.
-#   For a copy, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
-#   Author: shijie Sun
-#   Email: shijieSun@chd.edu.cn
-#   Github: www.github.com/shijieS
-#
+
 
 from .voc0712 import VOCDetection, VOCAnnotationTransform, VOC_CLASSES, VOC_ROOT
 
